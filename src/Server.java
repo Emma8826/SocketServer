20b06b94206570c7e86c114542a37b8d9b8a7acf
Emma8826 <j852641@gmail.com>
2022-08-29 14:21:38 +0800

    learnig thread

--- src/Server.java
+++ src/Server.java
@@ -12,14 +12,14 @@ public class Server {
     public void startServer(){
         try{
             while(!serverSocket.isClosed()){//while server socket is open,waitting client connected
-                Socket socket = serverSocket.accept();//meaning our program will be halted here until a client connects
+                Socket socket = serverSocket.accept();//meaning program will be halted here until a client connects
                 System.out.println("A new client is connected!");
                 
                 //each object of this class will be responsible for communication with a client
                 ClientHandler clientHandler = new ClientHandler(socket);//pass in socket
 
                 //if we didn't spawn a new thread to handle the connection with each new client 
-                //our application would only be able to handle one client at a time
+                //our application would only be able to handle one client at a time(cause need to multiple users connect)
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             }
